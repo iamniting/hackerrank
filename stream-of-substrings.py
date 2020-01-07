c369456874452f6c9dd16769a4472eededf44339
Nitin Goyal <nigoyal@redhat.com>
2020-01-07 22:25:11 +0530

    Add efficient sol of 'Stream of Substrings!'

--- stream-of-substrings.py
+++ stream-of-substrings.py
@@ -3,6 +3,41 @@
 import doctest
 
 
+def printSubStringEfficient(string):
+    """
+    >>> printSubString('abcd')
+    a
+    b
+    c
+    d
+    ab
+    bc
+    cd
+    abc
+    bcd
+    abcd
+    >>> printSubString('abcde')
+    a
+    b
+    c
+    d
+    e
+    ab
+    bc
+    cd
+    de
+    abc
+    bcd
+    cde
+    abcd
+    bcde
+    abcde
+    """
+    for i in range(1, len(string) + 1):
+        for j in range(0, len(string) + 1 - i):
+            print (string[slice(j, j+i)])
+
+
 def printSubString(string):
     """
     >>> printSubString('abcd')
@@ -43,4 +78,4 @@ if __name__ == '__main__':
     # doctest.testmod()
 
     s = input()
-    printSubString(s)
+    printSubStringEfficient(s)
