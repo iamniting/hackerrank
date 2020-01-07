1f5b8c6e11ad748110c15aed651be4c4a25bf282
Nitin Goyal <nigoyal@redhat.com>
2020-01-07 20:57:28 +0530

    Add sol of 'Stream of Substrings!'

---
+++ stream-of-substrings.py
@@ -0,0 +1,46 @@
+#!/bin/python3
+# https://www.hackerearth.com/pt-br/problem/algorithm/print-all-substrings/
+import doctest
+
+
+def printSubString(string):
+    """
+    >>> printSubString('abcd')
+    a
+    b
+    c
+    d
+    ab
+    bc
+    cd
+    abc
+    bcd
+    abcd
+    >>> printSubString('abcde')
+    a
+    b
+    c
+    d
+    e
+    ab
+    bc
+    cd
+    de
+    abc
+    bcd
+    cde
+    abcd
+    bcde
+    abcde
+    """
+    for i in range(1, len(string)+1):
+        for j in range(0, len(string)):
+            if j+i < len(string)+1:
+                print("".join(string[j:j+i]))
+
+
+if __name__ == '__main__':
+    # doctest.testmod()
+
+    s = input()
+    printSubString(s)
